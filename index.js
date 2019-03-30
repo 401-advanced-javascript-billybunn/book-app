38f418fedf191bc49fafbccc11be5f6306f61a1d
Billy Bunn <williammbunn@gmail.com>
2019-03-30 07:01:00 -0700

    moves route handling to routes.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 'use strict';
 
 require('dotenv').config();
+const pg = require('pg');
+
+// Database Setup
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => console.error(err));
+module.exports = client;
 
 require('./src/server.js').start(process.env.PORT);
