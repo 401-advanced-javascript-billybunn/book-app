369c359543792e96e5145c3e12936de9473cb56f
Billy Bunn <williammbunn@gmail.com>
2019-03-30 09:29:59 -0700

    adds logic to check env to connect to mongo or postgres

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 'use strict';
 
 require('dotenv').config();
-const pg = require('pg');
 
 // Database Setup
-const client = new pg.Client(process.env.DATABASE_URL);
-client.connect();
-client.on('error', err => console.error(err));
-module.exports = client;
+
+// Connects to postgreSQL
+const postgresConnect = () => {
+  const pg = require('pg');
+
+  const client = new pg.Client(process.env.DATABASE_URL);
+  client.connect();
+  client.on('error', err => console.error(err));
+  module.exports = client;
+}
+
+// Connects to MongoDB
+const mongoConnect = () => {
+  throw null;
+  // const mongoose = require('mongoose');
+
+  // const mongooseOptions = {
+  //   useNewUrlParser: true,
+  //   useCreateIndex: true,
+  // };
+  // mongoose.connect(process.env.MONGODB_URI, mongooseOptions);
+}
+
+// Looks at env variable and connects to appropriate database
+(process.env.DB === 'mongo' ? mongoConnect : postgresConnect)();
 
 require('./src/server.js').start(process.env.PORT);
