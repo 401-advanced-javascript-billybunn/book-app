1a24d5de90aa48877bcc78e6aa0672d486bb9306
Billy Bunn <williammbunn@gmail.com>
2019-03-30 17:52:24 -0700

    modularizes book class

--- src/api/routes/route-handlers/create-search.js
+++ src/api/routes/route-handlers/create-search.js
@@ -1,17 +1,18 @@
 'use strict';
 
 const superagent = require('superagent');
+const Book = require('../../../models/book-class.js')
 
-function Book(info) {
-  const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
+// function Book(info) {
+//   const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
 
-  this.title = info.title ? info.title : 'No title available';
-  this.author = info.authors ? info.authors[0] : 'No author available';
-  this.isbn = info.industryIdentifiers ? `ISBN_13 ${info.industryIdentifiers[0].identifier}` : 'No ISBN available';
-  this.image_url = info.imageLinks ? info.imageLinks.smallThumbnail : placeholderImage;
-  this.description = info.description ? info.description : 'No description available';
-  this.id = info.industryIdentifiers ? `${info.industryIdentifiers[0].identifier}` : '';
-}
+//   this.title = info.title ? info.title : 'No title available';
+//   this.author = info.authors ? info.authors[0] : 'No author available';
+//   this.isbn = info.industryIdentifiers ? `ISBN_13 ${info.industryIdentifiers[0].identifier}` : 'No ISBN available';
+//   this.image_url = info.imageLinks ? info.imageLinks.smallThumbnail : placeholderImage;
+//   this.description = info.description ? info.description : 'No description available';
+//   this.id = info.industryIdentifiers ? `${info.industryIdentifiers[0].identifier}` : '';
+// }
 
 module.exports = (request, response, next) => {
   console.log('create search');
